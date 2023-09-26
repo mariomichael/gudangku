6cdaef3b4482cd279da3e0e3dcde088c9147f70b
Mario <mario.michaeljs21@gmail.com>
2023-09-26 22:40:39 +0700

    mengimplementasi fungsi registrasi, login, dan logout, menghubungkan model Item dengan User, menambah detail informasi pengguna seperti username dan last login

--- main/models.py
+++ main/models.py
@@ -1,10 +1,12 @@
 # import datetime
 # from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
