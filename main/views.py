604e306f5b1d8e06f94779bdb1f71e40a520e0b4
Michael <mario.michaeljs21@gmail.com>
2023-10-04 07:54:13 +0700

    mengkustomisasi halaman login, register, dan tambah inventori serta membuat card sebagai tampilan lain dari table

--- main/views.py
+++ main/views.py
@@ -96,3 +96,26 @@ def increase_amount(request, id):
 
 def decrease_amount(request, id):
     data = Product.objects.filter(pk=id)
+
+def edit_product(request, id):
+    # Get product berdasarkan ID
+    product = Product.objects.get(pk = id)
+
+    # Set product sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get data berdasarkan ID
+    product = Product.objects.get(pk = id)
+    # Hapus data
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
